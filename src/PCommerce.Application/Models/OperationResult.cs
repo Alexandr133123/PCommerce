e7759cdc901331175b6975cd1bdc7c3919d7d8da
ArtemG1111 <artem.grishnev03@gmail.com>
2024-09-02 15:03:37 +0300

    PCommerce-8: Refactore Error flow logic (Add OperationResult class)

---
+++ src/PCommerce.Application/Models/OperationResult.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace PCommerce.Application.Models
+{
+    public class OperationResult
+    {
+        public bool IsSuccess { get; }
+
+        public string ErrorMessage { get; }
+
+        public bool IsFaulted => !IsSuccess;
+
+        protected OperationResult(bool isSuccess, string errorMessage)
+        {
+            IsSuccess = isSuccess;
+            ErrorMessage = errorMessage;
+        }
+        public static OperationResult Success()
+        {
+            return new OperationResult(true, null);
+        }
+        public static OperationResult Failure(string message)
+        {
+            return new OperationResult(false, message);
+        }
+    }
+}
