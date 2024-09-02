bcdb7bdf50410a5ec290872ae6d53ac241350159
OlexanderDOl <solenogo1234@gmail.com>
2024-09-02 18:18:40 +0300

    PAN-290 Refactore Error flow logic (Add OperationResult class)

---
+++ src/PCommerce.Application/Models/OperationResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCommerce.Application.Models
+{
+    public class OperationResult
+    {
+        readonly bool IsSuccess;
+        
+        readonly string ErrorMessage;
+
+        protected OperationResult(bool isSuccess, string errorMessage)
+        {
+            IsSuccess = isSuccess;
+
+            ErrorMessage = errorMessage;
+        }
+
+        public bool IsFaulted => !IsSuccess;
+
+        public static OperationResult Success()
+        {
+            return new OperationResult(true,string.Empty);
+        }
+
+        public static OperationResult Failure(string errorMessage)
+        {
+            return new OperationResult(false,errorMessage);
+        }
+    }
+}
